29c56b821084770b1e144b2745ca3cd0a1d07c10
lizz-kuzz <111276778+lizz-kuzz@users.noreply.github.com>
2022-10-03 16:32:30 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,55 +1,20 @@
-#include "utils.hpp"
-#include "stack.hpp"
-#include "error.hpp"
+#include "../include/file.hpp"
+#include "../include/sort.hpp"
 
-#include <stdio.h>
-#include <assert.h>
+int main() { 
+  const char *INPUT_FILE_WITH_TEXT = "/mnt/c/Users/User/Desktop/programs/shakespeare/res/shakespeare.txt";
+  const char *OUTPUT_FILE =          "/mnt/c/Users/User/Desktop/programs/shakespeare/res/shakespeare_write.txt";
 
-// TODO сделать свой ассерт, коды ошибок, понять как дамп сделать макросомч
+  poem shakespeare = {
+    .text = NULL,
+    .arr_str = NULL,
+    .NUMBER = 0,
+  };
 
+  read_file(INPUT_FILE_WITH_TEXT, &shakespeare);  
+  
+  sorting_and_print_to_file(&shakespeare, OUTPUT_FILE);
 
-int main() {
-    const char *LOG_FILE = "/mnt/c/Users/User/Desktop/programs/stack/log.txt";
-    // const char *LOG_FILE = "C://Users//User//Desktop//programs//stack//log.txt";
-    // extern FILE *log = fopen("/mnt/c/Users/User/Desktop/programs/stack/log.txt", "w");
-
-
-    FILE *log = fopen(LOG_FILE, "w");
-
-    assert(log != nullptr && "coudn't open file");
-
-    if (log == nullptr)
-        printf("Could not open file.\n");
-
-    stack stk1 = {
-        .data = nullptr,
-        .size = 0,
-        .capacity = 0,
-        // .number_of_error = 0,
-        .info = { 
-            0, 
-            __LINE__, 
-            __FUNCTION__, 
-            __FILE__, 
-        },
-        // .LINE = __LINE__,
-        // .FUNC = __FUNCTION__,
-        // .NAME_FILE = __FILE__, 
-    };
-
-    stack_ctor(&stk1, 10);
-    assert(stk1.data != nullptr && "null pointer");
-    to_dump(&stk1, log);
-    for (int i = 1; i <= 40; i++) {
-        stack_push(&stk1, i);
-        to_dump(&stk1, log);
-    }
-    elem_stk value = 0;
-    for (int i = 40; i > 0; i--) {
-        stack_pop(&stk1, &value);
-        to_dump(&stk1, log);
-    }
-    stack_dtor(&stk1);
-    fclose(log);
+  return 0;
 
 }
